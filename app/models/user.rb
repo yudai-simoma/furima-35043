eae267762e1b7f403f03fff5a408a536ed434fd1
yudai-simoma <cdcdasdll@icloud.com>
2021-03-28 12:13:53 +0900

    userモデルにバリデーションとアソシエーションを記載

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  #以下、それぞれの値が空では登録出来ないと言うバリデーションを持たせた
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birthday_date, presence: true
+      
+  #アソシエーションを記載
+  has_many :items
+  has_many :purchasers
+  has_many :comments
 end
