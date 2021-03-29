cd74dc8fc1ea9d27afb317b77fb8abf47a2e6156
yudai-simoma <cdcdasdll@icloud.com>
2021-03-29 17:06:26 +0900

    itemsコントローラーのアクションをルーティングに設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  #新規登録のdevise呼ぶ時のコード
   devise_for :users
   #トップページを表示させるコード
   root to: 'items#index'
+  #商品出品機能に必要なアクション
+  resources :items, only: [:new, :create]
 end
