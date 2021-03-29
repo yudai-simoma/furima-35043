ab4333df1d8fd2e6840ba68b522ce235cd03b17e
yudai-simoma <cdcdasdll@icloud.com>
2021-03-29 23:02:04 +0900

    itemsコントローラーのcreateアクション・ストロングパラメーターを設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,19 +1,27 @@
 class ItemsController < ApplicationController
   
+  #トップページを表示
   def index
   end
 
+  #商品出品ページを表示
   def new
     @item = Itme.new
   end
 
+  #商品出品の保存、保存されればトップページ、保存出来なければ商品出品ページ
   def create
-
+    @item = Itme.new(prototyoe_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render new_item_path
+    end
   end
 end
 
 private
 
-  def message_params
-    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  def item_params
+    params.require(:itme).permit(:product, :product_description, :category_id, :status_id, :ship_base_id, :prefecture_id, :ship_date_id, :price,:image).merge(user_id: current_user.id)
   end
