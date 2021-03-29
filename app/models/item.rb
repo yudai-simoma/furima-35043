30f70dc1b5755d0378d9ffab0dced80536f6cd77
yudai-simoma <cdcdasdll@icloud.com>
2021-03-29 17:00:20 +0900

    itemモデルにバリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,17 @@
 class Item < ApplicationRecord
+  # 以下、それぞれの値が空では登録出来ないと言うバリデーションを持たせた
+  with_options presence: true do
+    validates :product
+    validates :product_description
+    validates :category_id
+    validates :status_id
+    validates :ship_base_id
+    validates :prefecture_id
+    validates :ship_date_id
+    validates :price
+    validates :image
+  end
+
   #アソシエーションを追加
   belongs_to :user
   #画像ファイルの紐付け
