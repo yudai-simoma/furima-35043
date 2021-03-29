5e367f173f1c0a378b6462e7971ac253cb948974
yudai-simoma <cdcdasdll@icloud.com>
2021-03-29 19:19:46 +0900

    ActiveHashのモデルファイルを作成

---
+++ app/models/ship_date.rb
@@ -0,0 +1,12 @@
+#商品出品（発送までの日数）
+class ShipDate < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: '1~2日で発送' },
+    { id: 3, name: '2~3日で発送' },
+    { id: 3, name: '4~7日で発送' },
+  ]
+  #itemモデルとのアソシエーション
+  include ActiveHash::Associations
+  has_many :items
+end 
