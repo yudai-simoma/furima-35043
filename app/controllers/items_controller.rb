cf2fcd5f78636fc31f91b057c66c81197bf76269
yudai-simoma <cdcdasdll@icloud.com>
2021-03-29 17:07:28 +0900

    itemsコントローラーにnew及びcreateを定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,19 @@
 class ItemsController < ApplicationController
+  
   def index
   end
+
+  def new
+
+  end
+
+  def create
+
+  end
 end
+
+private
+
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
