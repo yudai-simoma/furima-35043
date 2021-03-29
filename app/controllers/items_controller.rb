bfb6430f101f81bc8c407f73f1f825e1adbb8c97
yudai-simoma <cdcdasdll@icloud.com>
2021-03-29 23:50:10 +0900

    エラーによる修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,12 +6,12 @@ class ItemsController < ApplicationController
 
   #商品出品ページを表示
   def new
-    @item = Itme.new
+    @item = Item.new
   end
 
   #商品出品の保存、保存されればトップページ、保存出来なければ商品出品ページ
   def create
-    @item = Itme.new(prototyoe_params)
+    @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else
@@ -23,5 +23,5 @@ end
 private
 
   def item_params
-    params.require(:itme).permit(:product, :product_description, :category_id, :status_id, :ship_base_id, :prefecture_id, :ship_date_id, :price,:image).merge(user_id: current_user.id)
+    params.require(:item).permit(:product, :product_description, :category_id, :status_id, :ship_base_id, :prefecture_id, :ship_date_id, :price,:image).merge(user_id: current_user.id)
   end
