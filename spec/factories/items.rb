ec015ffbe092f1daab8ab51fed93e014cd36ec13
yudai-simoma <cdcdasdll@icloud.com>
2021-03-30 12:15:02 +0900

    FactoryBotのitemファイル追加・テスト用画像追加

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+      product { 'テスト' }
+      product_description{ 'テスト中' }
+      category { 2 }
+      status { 2 }
+      ship_base { 2 }
+      prefecture { 2 }
+      ship_date { 2 }
+      price { 1000 }
+
+      #生成するダミーデータに画像を添付
+      after(:build) do |item|
+        item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+      end
   end
 end
