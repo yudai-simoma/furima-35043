f04a6e6205e9247cc12ea59cd7f2225214b49990
yudai-simoma <cdcdasdll@icloud.com>
2021-04-01 12:45:00 +0900

    destroyアクションとルーティングを追加・定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -46,6 +46,14 @@ class ItemsController < ApplicationController
         render :edit             
       end
   end
+
+  #出品した商品を削除する
+  def destroy
+    item = Item.find(params[:id])
+    if item.destroy
+      redirect_to root_path
+    end
+  end
 end
 
 private
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   #トップページを表示させるコード
   root to: 'items#index'
-  #商品出品機能に必要なアクション
-  resources :items, only: [:index, :new, :create, :show, :edit, :update]
+  #商品出品機能に必要な7つのアクション全て
+  resources :items
 end
