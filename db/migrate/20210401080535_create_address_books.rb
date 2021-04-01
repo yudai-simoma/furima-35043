79b1f9991ca5de33bd1c6c10691c6ad3abac9e0b
yudai-simoma <cdcdasdll@icloud.com>
2021-04-01 17:46:58 +0900

    purchasersテーブルとaddress_booksテーブルを作成

--- db/migrate/20210401080535_create_address_books.rb
+++ db/migrate/20210401080535_create_address_books.rb
@@ -1,7 +1,13 @@
 class CreateAddressBooks < ActiveRecord::Migration[6.0]
   def change
     create_table :address_books do |t|
-
+      t.string   :postal_code,   null: false
+      t.integer   :prefecture_id,   null: false
+      t.string   :city,   null: false
+      t.string   :house_number,   null: false
+      t.string   :building_name
+      t.string   :phone_number,   null: false
+      t.references :purchaser, null: false, foreign_key: true
       t.timestamps
     end
   end
