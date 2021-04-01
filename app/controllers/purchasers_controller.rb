0605dd49e3d0143c71aba6c3a89b058142d37f03
yudai-simoma <cdcdasdll@icloud.com>
2021-04-02 00:13:05 +0900

    index・createアクションを設定

--- app/controllers/purchasers_controller.rb
+++ app/controllers/purchasers_controller.rb
@@ -5,10 +5,25 @@ before_action :authenticate_user!, except: :index
   #商品購入ページを表示
   def index
     @item = Item.find(params[:item_id])
+    # form_withのmodelオプションで指定することができるコード
+    @purchaser_address = PurchaserAddress.new
   end
 
   #商品購入の保存
   def create
-
+    @purchaser_address = PurchaserAddress.new(purchaser_params)
+    #バリデーションを実行する機能がないため,バリデーションを持たせる
+    if @purchaser_address.valid?
+      @purchaser_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 end
+
+  private
+
+  def purchaser_params
+    params.require(:purchaser_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
