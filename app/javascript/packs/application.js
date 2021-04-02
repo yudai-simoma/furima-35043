7b8d20398a1cd8e3ab9b54fc8dbdb9c0a1437a39
yudai-simoma <cdcdasdll@icloud.com>
2021-04-02 18:22:13 +0900

    card.jsファイルを呼び出せるように実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,8 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 //item_price.jsを読み込む記述
-require("../item_price");
+require("../item_price")
+require("../card")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
