8a0be7c787318334fa36a27ca46fb3d90a70ba9c
yudai-simoma <cdcdasdll@icloud.com>
2021-04-02 14:17:30 +0900

    商品購入のテストコード作成

--- spec/factories/purchaser_addresses.rb
+++ spec/factories/purchaser_addresses.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :purchaser_address do
-    
+    postal_code { "123-4567" }
+    prefecture_id { 2 }
+    city { '新宿区' }
+    house_number { '1丁目1-1' }
+    building_name { 'ハイムふじ' }
+    phone_number { '09012345678' }
   end
 end
