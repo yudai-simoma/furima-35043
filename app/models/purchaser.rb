24f368abe167f4ea4a33856243f56bba41005768
yudai-simoma <cdcdasdll@icloud.com>
2021-04-02 21:50:44 +0900

    tokenをpurchaser_addressモデルで使用できるようにした

--- app/models/purchaser.rb
+++ app/models/purchaser.rb
@@ -4,10 +4,6 @@ class Purchaser < ApplicationRecord
   belongs_to :user
   belongs_to :item
   has_one    :address_book
-
-  # tokenの値をPurchaserモデルで取り扱えるようにしましょう
-  attr_accessor :token
-  
   # ActiveHashのアソシエーションを設定
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
--- app/models/purchaser_address.rb
+++ app/models/purchaser_address.rb
@@ -1,20 +1,20 @@
 class PurchaserAddress
   #ヘルパーメソッドの引数として扱え、バリデーションの機能を使用できるようにするコード
   include ActiveModel::Model
-  #purchasersテーブルとaddressesテーブルに保存したいカラム名を、すべて指定
-  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id
+  #purchasersテーブルとaddressesテーブルに保存したいカラム名を、すべて指定またtokenの値も取り扱えるようにした
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id, :token
 
   #purchasersモデルとaddressesモデルのバリデーションを記述
   with_options presence: true do
     #アソシエーションが組めないためバリデーションを記載
     validates :user_id
     validates :item_id
-    #validates :purchaser_id
     validates :postal_code, format: { with:/\A\d{3}[-]\d{4}\z/, message: 'is invalid. Input correctly'}
     validates :prefecture_id
     validates :city
     validates :house_number
     validates :phone_number, format: { with:/\A\d{11}\z/, message: 'is invalid. Input only number'}
+    validates :token
   end
 
   #フォームから送られてきたデータをテーブルに保存する処理
