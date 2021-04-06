00272b4b9765b77641eaf7350a221a103a41913b
yudai-simoma <cdcdasdll@icloud.com>
2021-04-06 21:03:53 +0900

    Revert "商品出品機能の改善"
    This reverts commit 178a2785f183b3f2b2f1fd71db4b30807bd22050.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,7 +11,6 @@ require("channels")
 //item_price.jsを読み込む記述
 require("../item_price")
 require("../card")
-require('../preview')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
